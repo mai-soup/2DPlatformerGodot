40b0bd21e0472bc4100d67ed0bc0ae90b7c6584b
Maijs Garais <maireadsbooks@gmail.com>
2022-11-21 17:52:57 +0000

    basic jump logic

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,9 +1,8 @@
 extends Actor
 
 func _physics_process(delta: float) -> void:
-	var direction: = get_direction()
-	velocity = speed * direction
-	velocity = move_and_slide(velocity)
+	velocity = calculate_move_velocity(velocity, get_direction(), speed)
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func get_direction() -> Vector2:
 	# moving to the right is 1.0, to the left is -1.0
@@ -11,3 +10,19 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2
+		) -> Vector2:
+	var new_velocity = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	
+	# if is jumping
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	else:
+		new_velocity.y += gravity * get_physics_process_delta_time()
+		new_velocity.y = min(new_velocity.y, speed.y)
+	return new_velocity
