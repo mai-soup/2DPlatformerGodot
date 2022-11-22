b0400d0da9269d615b9ffa22c9fb734ef08f120c
Maijs Garais <maireadsbooks@gmail.com>
2022-11-22 16:48:55 +0000

    make enemy killable by jumping on top of it

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -6,6 +6,15 @@ func _ready() -> void:
 	set_physics_process(false)
 	# initially move left, towards the player
 	_velocity.x = -speed.x
+	
+func _on_StompDetector_body_entered(body: Node) -> void:
+	# if the player is below the detector, do nothing
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	
+	# otherwise, make sure enemy can't be collided with and delete it
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
 
 func _physics_process(delta: float) -> void:
 	# fall if not on ground
