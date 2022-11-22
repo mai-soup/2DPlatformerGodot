b9bf349f63c19741824b1d0d5399a967dbd81d53
Maijs Garais <maireadsbooks@gmail.com>
2022-11-22 18:30:05 +0000

    make player bounce off top of enemy, enable player to be killed by enemy

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,13 @@
 extends Actor
 
+export var stomp_impact: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impact)
+
+func _on_EnemyDetector_body_entered(body: Node) -> void:
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	_velocity = calculate_move_velocity(_velocity, get_direction(), speed, is_jump_interrupted)
@@ -30,4 +38,9 @@ func calculate_move_velocity(
 	else:
 		new_velocity.y += gravity * get_physics_process_delta_time()
 		new_velocity.y = min(new_velocity.y, speed.y)
+	return new_velocity
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impact: float) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.y = -impact
 	return new_velocity
