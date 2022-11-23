6ca1e698494e4a11525ba7631bab70c2668c733d
Maijs Garais <maireadsbooks@gmail.com>
2022-11-23 20:25:30 +0000

    create portal script, teleportation logic

---
+++ src/Objects/Portal.gd
@@ -0,0 +1,21 @@
+tool
+extends Area2D
+
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+export var next_scene: PackedScene
+
+func _on_body_entered(body: Node) -> void:
+	teleport()
+	
+func _get_configuration_warning() -> String:
+	if not next_scene:
+		return "'Next Scene' can't be empty."
+	else:
+		return ""
+
+func teleport() -> void:
+	anim_player.play("fade_in")
+	# wait for animation to finish before switching scenes
+	yield(anim_player, "animation_finished")
+	get_tree().change_scene_to(next_scene)
