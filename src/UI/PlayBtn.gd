c3cdde3173b2575a9096cbf627d51f1598351b22
Maijs Garais <maireadsbooks@gmail.com>
2022-11-24 16:50:20 +0000

    add menu buttons and a working title menu

---
+++ src/UI/PlayBtn.gd
@@ -0,0 +1,13 @@
+tool
+extends Button
+
+export(String, FILE) var next_scene_path: = ""
+
+func _on_button_up() -> void:
+	get_tree().change_scene(next_scene_path)
+
+func _get_configuration_warning() -> String:
+	if next_scene_path == "":
+		return "Next scene path must be set"
+	else:
+		return ""
